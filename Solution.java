2d1bd1a13ad7beb1e161eaf8f406a09de8367536
Anumit Jooloor <anumit@gmail.com>
2024-04-14 13:38:15 -0700

    Add files

---
+++ Solution.java
@@ -0,0 +1,25 @@
+/*
+rm *.class
+/usr/local/Cellar/openjdk/21.0.2/bin/javac *.java
+/usr/local/Cellar/openjdk/21.0.2/bin/java Main
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    List<Integer> ls = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        if (root == null)
+            return new ArrayList<>();
+
+        inorderTraversal(root.left);
+        ls.add(root.val);
+        inorderTraversal(root.right);
+
+        return ls;
+    }
+}
